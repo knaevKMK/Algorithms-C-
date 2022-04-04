e4b0baec32120d131e5a2c9f0acc258f1c782906
knaevKMK <knev@mail.bg>
2022-04-04 18:26:07 +0300

    Add project files.

---
+++ Algorithm/Algorithm/Program.cs
@@ -0,0 +1,30 @@
+﻿using Struct;
+
+Graph g = new Graph(8);
+
+g.AddEdge(0, 1);
+g.AddEdge(0, 2);
+g.AddEdge(2, 0);
+
+g.AddEdge(1, 3);
+g.AddEdge(1, 4);
+
+g.AddEdge(2, 5);
+
+g.AddEdge(4, 5);
+g.AddEdge(4, 6);
+g.AddEdge(4, 7);
+
+
+int key = int.Parse(Console.ReadLine());
+Console.Write("Following is Breadth First " +
+              "Traversal(starting from " +
+             $"vertex {key})\n");
+g.BFS(key);
+
+Console.WriteLine(
+           "\nFollowing is Depth First Traversal "
+           + $"(starting from vertex {key})");
+
+g.DFS(key);
+Console.ReadKey();
---
+++ Algorithm/Struct/Graph.cs
@@ -0,0 +1,88 @@
+﻿namespace Struct
+{
+    public class Graph
+    {
+        private int V;
+
+        LinkedList<int>[] Adjacency { get; set; }
+
+
+
+        public Graph(int v)
+        {
+            this.Adjacency = new LinkedList<int>[v];
+
+            for (int i = 0; i < Adjacency.Length; i++)
+            {
+                Adjacency[i] = new LinkedList<int>();
+            }
+
+            this.V = v;
+        }
+        public void AddEdge(int v, int w)
+        {
+            this.Adjacency[v].AddLast(w);
+        }
+
+
+
+        private void BSFUtil(LinkedList<int> queue, bool[] visited)
+        {
+            var s = queue.First();
+            Console.Write(s + " ");
+            queue.RemoveFirst();
+
+            LinkedList<int> list = this.Adjacency[s];
+
+            foreach (var val in list)
+            {
+                if (!visited[val])
+                {
+                    visited[val] = true;
+                    queue.AddLast(val);
+                }
+            }
+
+            if (queue.Any())
+            {
+                BSFUtil(queue, visited);
+            }
+        }
+
+        public void BFS(int s)
+        {
+            bool[] visited = new bool[this.V];
+            LinkedList<int> queue = new();
+
+            visited[s] = true;
+            queue.AddLast(s);
+
+            BSFUtil(queue, visited);
+
+        }
+
+
+
+        void DFSUtil(int v, bool[] visited)
+        {
+            visited[v] = true;
+            Console.Write(v + " ");
+
+            LinkedList<int> vList = this.Adjacency[v];
+            foreach (var n in vList)
+            {
+                if (!visited[n])
+                    DFSUtil(n, visited);
+            }
+        }
+
+        public void DFS(int v)
+        {
+            bool[] visited = new bool[V];
+
+            DFSUtil(v, visited);
+        }
+
+
+    }
+}
